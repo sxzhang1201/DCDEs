6e7d53e829788bcabcd191c96be978615ade6418
Shuxin Zhang <s.x.zhang1201@gmail.com>
2021-05-20 15:17:10 +0200

    Revert "Update Comments"
    This reverts commit b465903ec63f69a455d288ec4c5a35bb56edcde4.

--- run_preprocessing.py
+++ run_preprocessing.py
@@ -40,31 +40,22 @@ for ern_name in ern_list:
     # Remove special characters
     text = remove_characters(text)
 
-    # Tokenize texts into tokens
+    # tokenize
     tokens = word_tokenize(text)
 
-    # Stemming
+    # stemming
     tokens2 = [lem.lemmatize(token) for token in tokens]
 
-    # Rejoin tokens into texts
     text = " ".join(tokens2)
 
-    # Add texts to output list
     ern_dic.append(text)
 
-# Initiate a vectorizer, and there are many parameters to be adjusted according to your needs
 vec = CountVectorizer(min_df=2, lowercase=True, stop_words=stopwords, binary=True)
 
-# Vectorizing texts
 X = vec.fit_transform(ern_dic)
 
-# Transform results into dataframe
 df_term = pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
 
-# Rename labels with ERN names
-df_term = df_term.set_axis(ern_list, axis=0)
-
-# Export as csv
 df_term.to_csv("df_term_binary.csv")
 
 
